60f0d58c184283af71a01eb7d9aa99c5bc9ab7c8
katxrezzam <katxrezzam@hotmail.com>
2023-02-14 05:08:05 -0500

    removing build folder

--- build/lib/cors/allowedOrigins.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.allowedOrigins = void 0;
-exports.allowedOrigins = [
-    'http://localhost:3500',
-    'http://localhost:3000'
-];
--- build/util/errorLogger.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.errorMessage = void 0;
-const errorMessage = (error) => {
-    const result = error.message;
-    return result;
-};
-exports.errorMessage = errorMessage;
